ddbbbe5b1d52c92fbc2c9bcb721b0487d0f41a73
DegirmenciOmer <rumidegirmenci0@gmail.com>
2023-12-05 23:22:47 +0100

    sprite

--- sticky.js
+++ sticky.js
@@ -1,1 +1,83 @@
-"use strict";!function(e){"function"==typeof define&&define.amd?define(e):"undefined"!=typeof module&&module.exports?module.exports=e():window.enterView=e.call(this)}(function(){var e=function(e){function n(){g=window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||window.msRequestAnimationFrame||function(e){return setTimeout(e,1e3/60)}}function t(){if(h&&"number"==typeof h){var e=Math.min(Math.max(0,h),1);return q-e*q}return q}function i(){var e=document.documentElement.clientHeight,n=window.innerHeight||0;q=Math.max(e,n)}function o(){y=!1;var e=t();A=A.filter(function(n){var t=n.getBoundingClientRect(),i=t.top,o=i<e;if(o&&!n.__enter_view){if(m(n),_)return!1}else!o&&n.__enter_view&&w&&w(n);return n.__enter_view=o,!0}),A.length||window.removeEventListener("scroll",r,!0)}function r(){y||(y=!0,g(o))}function u(){i(),o()}function f(e){for(var n=e.length,t=[],i=0;i<n;i+=1)t.push(e[i]);return t}function c(e){var n=arguments.length>1&&void 0!==arguments[1]?arguments[1]:document;return"string"==typeof e?f(n.querySelectorAll(e)):e instanceof NodeList?f(e):e instanceof Array?e:void 0}function d(){A=c(l)}function a(){window.addEventListener("resize",u,!0),window.addEventListener("scroll",r,!0),u()}function s(){var e=l&&m;e||console.error("must set selector and enter options"),n(),d(),a(),o()}var l=e.selector,m=e.enter,w=e.exit,v=e.offset,h=void 0===v?0:v,p=e.once,_=void 0!==p&&p,g=null,y=!1,A=[],q=0;s()};return e});
+// Constructor function for creating ScrollVideo instances
+function ScrollVideo(elem) {
+  // Get the video and canvas elements within the provided elem
+  const video = elem.querySelector('video')
+  const canvas = elem.querySelector('canvas')
+
+  // Get the 2D rendering context for the canvas
+  const ctx = canvas.getContext('2d')
+
+  // Initialize variables for video duration and seeking state
+  let videoDuration = 0.001
+  let seeked = true
+
+  // Function to update the video duration when it changes
+  function setVideoDuration() {
+    console.log('ondurationchange')
+    videoDuration = video.duration
+  }
+
+  // Function that loops for smooth video scrolling effect
+  function loop() {
+    // Request animation frame to continuously call the loop function
+    requestAnimationFrame(loop)
+    console.log(requestAnimationFrame(loop))
+
+    // Check if seeking is allowed
+    if (seeked) {
+      seeked = false // Set seeked to false to prevent continuous seeking
+
+      // Get scroll information
+      const { scrollHeight, clientHeight, scrollTop } = document.body
+      const maxScroll = scrollHeight - clientHeight
+      const scrollProgress = scrollTop / Math.max(maxScroll, 1)
+
+      // Set canvas dimensions to match the video
+      canvas.width = video.videoWidth
+      canvas.height = video.videoHeight
+
+      // Calculate and set the video time based on scroll position
+      if (!isNaN(video.duration)) {
+        video.currentTime = video.duration * scrollProgress
+        // console.log(video.currentTime)
+      }
+
+      // Clear the canvas and draw the video frame
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+    }
+  }
+
+  // Initialize the video player
+  function init() {
+    // Set the loop function to start playing the video on canplay event
+    video.oncanplay = loop
+
+    // Set initial video time for scrolling
+    video.currentTime = 0.001
+
+    // Initialize the observer for seeking
+    initObserver()
+  }
+
+  // Function to handle seeking events
+  function initObserver() {
+    video.addEventListener('seeked', () => {
+      seeked = true // Allow seeking when the video has seeked
+    })
+  }
+
+  // Expose the init function as a public method
+  return {
+    init,
+  }
+}
+
+// Get all elements with class 'canvas-wrapper'
+const components = document.querySelectorAll('.canvas-wrapper')
+
+// Create ScrollVideo instances for each element and initialize
+components.forEach((item) => {
+  const component = ScrollVideo(item)
+  component.init()
+})
